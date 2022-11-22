47eb5256bbed87b4858e88bb932f73f143b74b26
Rodrigo Bruno <rodrigobrunols@gmail.com>
2022-11-22 16:16:59 -0300

    98

---
+++ src/main/java/org/lab/solutions/leetcode/_98_Validate_BST.java
@@ -0,0 +1,51 @@
+package org.lab.solutions.leetcode;
+
+public class _98_Validate_BST {
+
+    /**
+     * Definition for a binary tree node.
+     *
+     */
+      private class TreeNode {
+          int val;
+          TreeNode left;
+          TreeNode right;
+          TreeNode() {}
+          TreeNode(int val) { this.val = val; }
+          TreeNode(int val, TreeNode left, TreeNode right) {
+              this.val = val;
+              this.left = left;
+              this.right = right;
+          }
+      }
+
+    //Top Down DFS
+    public boolean isValidBST(TreeNode root) {
+
+        return preeorder(root, null, null);
+    }
+
+
+    /**
+     * Time O(n)
+     * Space O(n)
+     *
+     * @param root
+     * @param low
+     * @param high
+     * @return true if is a valide BST
+     */
+    public boolean preeorder(TreeNode root, Integer low, Integer high){
+        if(root == null) return true;
+
+        //process
+        if ( (low != null && root.val <= low) || (high != null && root.val >= high)) return false;
+
+        return preeorder(root.left, low, root.val) &&
+                preeorder(root.right, root.val, high);
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
